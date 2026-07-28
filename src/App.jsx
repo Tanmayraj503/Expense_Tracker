d875d9f5174d31112c5312f9ff03231ca8fdbd1b
unknown <tanmayraj707@gmail.com>
2026-07-28 22:01:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-6xl text-yellow-900'>hello there!</h1>
+    </>
+  )
+}
+
+export default App
