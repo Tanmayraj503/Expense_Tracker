9bd4eab0fc7e084950d5c413ecca96403069ef04
unknown <tanmayraj707@gmail.com>
2026-08-12 23:04:55 +0530

    line graph

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DarkModeToggle from './components/darkmodetoggle'
 import ScrollToTopButton from './components/ScrollToTopButton'
 import PieChart from './components/piechart'
 import Landing from './pages/landing'
+import LineChart from './components/linegraph'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <ScrollToTopButton />
         <ExpenseButton />
         {/* <PieChart /> */}
+        <LineChart/>
         <Landing />
         <div className="fixed top-5 right-5">
           <DarkModeToggle />
